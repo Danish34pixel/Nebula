6e61d38d24a1bd9d723d07e29558fac8cb79ff90
Divyansh Duttaroy <divyanshduttaroy163@gmail.com>
2026-06-01 12:56:20 +0530

    feat: update app version to 50 in app.config.js

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ module.exports = {
   expo: {
     name: "Meditrap",
     slug: "Meditrap",
-    version: "48",
+    version: "50",
     orientation: "portrait",
     icon: "./assets/images/app-icon.png",
     scheme: "meditrap",
